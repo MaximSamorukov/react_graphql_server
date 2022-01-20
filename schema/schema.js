b60a1c54c128a5da7898fe07e5d0583843b6ffb0
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-20 20:00:35 +0300

    wip

--- schema/schema.js
+++ schema/schema.js
@@ -6,11 +6,58 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((response) => response.data);
+      }
+    }
+  })
+});
+
+const PositionType = new GraphQLObjectType({
+  name: 'Position',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    salary: {
+      type: GraphQLInt,
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/positions/${parentValue.id}/users`)
+          .then((response) => response.data);
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: {
       type: GraphQLString,
     },
@@ -20,8 +67,27 @@ const UserType = new GraphQLObjectType({
     age: {
       type: GraphQLInt,
     },
-  }
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((response) => response.data);
+      },
+    },
+    position: {
+      type: PositionType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/positions/${parentValue.positionId}`)
+          .then((response) => response.data);
+      },
+    },
+  }),
 });
+
+
+
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -39,6 +105,32 @@ const RootQuery = new GraphQLObjectType({
           .then((response) => response.data);
       },
     },
+    company: {
+      type: CompanyType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((response) => response.data);
+      },
+    },
+    position: {
+      type: PositionType,
+      args: {
+        id: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/positions/${args.id}`)
+          .then((response) => response.data);
+      },
+    }
   },
 });
 
