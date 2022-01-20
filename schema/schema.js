784dc4c98bc3e9ee5b73a03ccf741478e67cac04
Maxim Samorukov <maxim.samorukov@gmail.com>
2022-01-20 21:49:01 +0300

    added deleted and updated

--- schema/schema.js
+++ schema/schema.js
@@ -7,6 +7,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -86,9 +87,6 @@ const UserType = new GraphQLObjectType({
   }),
 });
 
-
-
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -134,6 +132,189 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        age: {
+          type: new GraphQLNonNull(GraphQLInt),
+        },
+        companyId: {
+          type: GraphQLString,
+        },
+        positionId: {
+          type: GraphQLString,
+        }
+      },
+      resolve(parentValue, { age, firstName, companyId, positionId }) {
+        return axios
+          .post(`http://localhost:3000/users`, {
+            age,
+            firstName,
+            positionId,
+            companyId
+          })
+          .then((res) => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/users/${args.id}`)
+          .then((res) => res.data);
+      }
+    },
+    deleteCompany: {
+      type: CompanyType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/companies/${args.id}`)
+          .then((res) => res.data);
+      }
+    },
+    deletePosition: {
+      type: PositionType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/positions/${args.id}`)
+          .then((res) => res.data);
+      }
+    },
+    addCompany: {
+      type: CompanyType,
+      args: {
+        name: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        description: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(parentValue, { description, name }) {
+        return axios
+          .post(`http://localhost:3000/companies`, {
+            name,
+            description,
+          })
+          .then((res) => res.data);
+      }
+    },
+    addPosition: {
+      type: PositionType,
+      args: {
+        title: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        salary: {
+          type: new GraphQLNonNull(GraphQLInt),
+        },
+      },
+      resolve(parentValue, { title, salary }) {
+        return axios
+          .post(`http://localhost:3000/positions`, {
+            title,
+            salary,
+          })
+          .then((res) => res.data);
+      }
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        firstName: {
+          type: GraphQLString,
+        },
+        age: {
+          type: GraphQLInt,
+        },
+        companyId: {
+          type: GraphQLString,
+        },
+        positionId: {
+          type: GraphQLString,
+        }
+      },
+      resolve(parentValue, args) {
+        const { id, ...rest } = args;
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, {
+            ...rest
+          })
+          .then((res) => res.data);
+      }
+    },
+    updateCompany: {
+      type: CompanyType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        name: {
+          type: GraphQLString,
+        },
+        description: {
+          type: GraphQLString,
+        },
+      },
+      resolve(parentValue, args) {
+        const { id, ...rest } = args;
+        return axios
+          .patch(`http://localhost:3000/companies/${args.id}`, {
+            ...rest
+          })
+          .then((res) => res.data);
+      }
+    },
+    updatePosition: {
+      type: PositionType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        title: {
+          type: GraphQLString,
+        },
+        salary: {
+          type: GraphQLInt,
+        },
+      },
+      resolve(parentValue, args) {
+        const { id, ...rest } = args;
+        return axios
+          .patch(`http://localhost:3000/positions/${args.id}`, {
+            ...rest
+          })
+          .then((res) => res.data);
+      }
+    },
+  }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 })
